6d135a1c052ec4e10ad3854252fb8cdceca80a42
satyamrajawat-1 <satyamrajawat283123@gmail.com>
2025-11-29 22:12:01 +0530

    changed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,14 @@ const app = express()
 
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+  origin: "https://taskify-mu-opal.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 
 app.use(express.json({
     limit:"16kb"
